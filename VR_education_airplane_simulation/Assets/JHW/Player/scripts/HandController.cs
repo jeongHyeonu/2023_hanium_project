697527df2b04f8dd69138f47b4e310da16a5a5ea
jeongHyeonu <114675420+jeongHyeonu@users.noreply.github.com>
2023-06-07 14:53:45 +0900

    Feat : 구명조끼 및 씬 추가
    .

--- VR_education_airplane_simulation/Assets/JHW/Player/scripts/HandController.cs
+++ VR_education_airplane_simulation/Assets/JHW/Player/scripts/HandController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandController : MonoBehaviour
 {
