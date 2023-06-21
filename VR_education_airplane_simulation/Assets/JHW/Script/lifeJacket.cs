2496a5ff64bf1e5d7ab53aac1e6e6586e6c92911
jeongHyeonu <114675420+jeongHyeonu@users.noreply.github.com>
2023-06-21 17:18:13 +0900

    Fix : 구명조끼 상호작용
    .

--- VR_education_airplane_simulation/Assets/JHW/Script/lifeJacket.cs
+++ VR_education_airplane_simulation/Assets/JHW/Script/lifeJacket.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -11,14 +12,23 @@ public class lifeJacket : MonoBehaviour
     [SerializeField] GameObject equipedJacket;
     [SerializeField] List<GameObject> beltPos;
 
-    public void EquipJacket()
-    {
-        equipedJacket.SetActive(true);
-        this.gameObject.SetActive(false);
-    }
+    [SerializeField] GameObject GrabUX;
+    [SerializeField] GameObject lifeJacketDropPos;
+    [SerializeField] TextMeshProUGUI txt;
 
-    public void ChangeHandPos()
+
+    public void JacketSelectEntered(GameObject _obj)
     {
+        lifeJacketDropPos.SetActive(true);
+        this.transform.position = _obj.transform.position;
+        txt.text = "앞의 승객에 구명조끼를 입혀주세요.";
+    }
+    public void JacketSelectExited()
+    {
+        lifeJacketDropPos.SetActive(false);
+        this.gameObject.SetActive(false);
+        equipedJacket.SetActive(true);
+        txt.text = "구명조끼의 벨트를 메주세요.";
 
     }
 
@@ -29,6 +39,9 @@ public class lifeJacket : MonoBehaviour
 
     public void JacketBeltSelectEntered()
     {
+        // UX OFF
+        GrabUX.SetActive(false);
+
         // 시작점 끝점 위치 파악해서 벨트 재조정
         Vector3 startPos = beltPos[0].transform.position;
         Vector3 destPos = beltPos[beltPos.Count-1].transform.position;
@@ -37,12 +50,16 @@ public class lifeJacket : MonoBehaviour
         // 벨트 위치 재조정
         for (int i = 1; i < beltPos.Count-2; i++)
         {
-            Debug.Log(beltPos[i].transform.position);
+            //Debug.Log(diff);
+            //Debug.Log(beltPos[i].transform.position);
+            Vector3 newPos = beltPos[i].transform.position - diff/i;
+            Debug.Log(newPos);
+            beltPos[i].transform.position = newPos;
         }
     }
 
     public void JacketBeltSelectExited()
     {
-
+        GrabUX.SetActive(true);
     }
 }
