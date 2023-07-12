c32eaf7ef325205f8b7e9a9bacbb4b488f31ff54
jeongHyeonu <114675420+jeongHyeonu@users.noreply.github.com>
2023-07-12 18:03:47 +0900

    Fix : 씬 수정 및 씬 이동 오류 수정
    .

--- VR_education_airplane_simulation/Assets/JHW/Script/MenuManager.cs
+++ VR_education_airplane_simulation/Assets/JHW/Script/MenuManager.cs
@@ -11,6 +11,9 @@ using UnityEngine.SceneManagement;
 // 메뉴화면 버튼
 partial class MenuManager : MonoBehaviour
 {
+    [SerializeField] GameObject mainTitle;
+    [SerializeField] GameObject chapterSelect;
+
     // 튜토리얼 버튼 클릭시
     public void MenuButton_Tutorial_OnMouseClick()
     {
@@ -18,4 +21,37 @@ partial class MenuManager : MonoBehaviour
         SceneManager.LoadScene("Tutorial");
         SoundManager.Instance.PlaySFX(SoundManager.SFX_list.button2);
     }
+
+    // 필수 안전교육 버튼 클릭시
+    public void MenuButton_AssentialEdu_OnMouseClick()
+    {
+        mainTitle.SetActive(false);
+        chapterSelect.SetActive(true);
+    }
+
+    // 뒤로 버튼
+    public void MenuButton_Back_OnMouseClick()
+    {
+        mainTitle.SetActive(true);
+        chapterSelect.SetActive(false);
+    }
+
+    // Chapter 3
+    public void MenuButton_Chapter3_OnMouseClick()
+    {
+        //SceneManager.SetActiveScene(SceneManager.GetSceneByName("EduGuideSafetyRules"));
+        // 버튼 클릭시 실행할 함수
+        SceneManager.LoadScene("EduGuideSafetyRules");
+        SoundManager.Instance.PlaySFX(SoundManager.SFX_list.button2);
+    }
+
+    // Chapter 5
+    public void MenuButton_Chapter5_OnMouseClick()
+    {
+        //SceneManager.SetActiveScene(SceneManager.GetSceneByName("EduLifeJacket"));
+        // 버튼 클릭시 실행할 함수
+        SceneManager.LoadScene("EduLifeJacket");
+        SoundManager.Instance.PlaySFX(SoundManager.SFX_list.button2);
+    }
+
 }
