f89e113c934ea45a476da7b18767775c1f2fbc74
jeongHyeonu <114675420+jeongHyeonu@users.noreply.github.com>
2023-07-26 18:40:53 +0900

    feat: chapter6, chapter7 개발 완료
    .

--- VR_education_airplane_simulation/Assets/JHW/Player/scripts/PlayerMove.cs
+++ VR_education_airplane_simulation/Assets/JHW/Player/scripts/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -13,8 +14,11 @@ public class PlayerMove : MonoBehaviour
     bool isKeyDown;
     Vector3 dir;
 
+    bool isMoveAble;
+
     private void Start()
     {
+        isMoveAble = true;
     }
 
     private void Update()
@@ -27,23 +31,36 @@ public class PlayerMove : MonoBehaviour
 
         float X_Move = Input.GetAxisRaw("Oculus_GearVR_RThumbstickX");
         float Y_Move = Input.GetAxisRaw("Oculus_GearVR_RThumbstickY");
-        Debug.Log(X_Move + ", " + Y_Move);
-        if(X_Move==0 && Y_Move==0) {  isKeyDown = false; }
+        
+        // 조이스틱 살짝만 누른경우 이동하지 않은걸로 판정
+        if(Mathf.Abs(X_Move) <= .5f && Mathf.Abs(Y_Move) <= .5f) {  isKeyDown = false; }
         else { dir = new Vector3 (X_Move,0, Y_Move); isKeyDown = true; }
 
         // 이동 및 회전
         if (isKeyDown)
         {
-            playerXROrigin.transform.position += playerSpeed * Time.deltaTime * dir;
-            playerXROrigin.transform.rotation = Quaternion.Lerp(playerXROrigin.transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotateSpeed);
+            dir = transform.TransformDirection(dir);    // 로컬 벡터를 월드 벡터로
 
-            //playerXROrigin.transform.position += dir * playerSpeed * Time.deltaTime;
-            //playerXROrigin.transform.rotation = Quaternion.Lerp(playerXROrigin.transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotateSpeed);
-            // 카메라
-            //cam.transform.position = this.transform.position + offsetVector;
-            //CamOffset.transform.position = this.transform.position + offsetVector;
+            // 회전
+            Vector3 rotationDirection = dir.normalized;
+            if (rotationDirection != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(rotationDirection);
+                playerXROrigin.transform.rotation = Quaternion.Slerp(playerXROrigin.transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+            }
+
+            // 이동
+            if(isMoveAble) playerXROrigin.transform.position += dir * playerSpeed * Time.deltaTime;
         }
-
-
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isMoveAble = false;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isMoveAble = true;
+    }
+
 }
