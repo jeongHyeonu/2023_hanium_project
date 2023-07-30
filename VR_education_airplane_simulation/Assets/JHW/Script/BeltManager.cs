f0a0d4d9b23c899424d41e8ed243132fee5d2f1b
jeongHyeonu <114675420+jeongHyeonu@users.noreply.github.com>
2023-07-30 12:13:43 +0900

    feat: 비행기 좌석 모델링 완료
    fix: :airplane: 씬 전부 수정 (skybox 및 기존 좌석 삭제)

--- VR_education_airplane_simulation/Assets/JHW/Script/BeltManager.cs
+++ VR_education_airplane_simulation/Assets/JHW/Script/BeltManager.cs
@@ -38,7 +38,7 @@ partial class BeltManager : MonoBehaviour
             case 3: // 승무원 설명 다 하면 벨트 오브젝트 활성화, 다음 버튼 비활성화, 승무원캠 off
                 belt1.SetActive(true);
                 belt2.SetActive(true);
-                cam.gameObject.SetActive(false);
+                //cam.gameObject.SetActive(false);
                 break;
             case 6: // ux 및 상호작용 활성화
                 beltEndPos2.GetComponent<XRGrabInteractable>().enabled = true;
@@ -76,7 +76,7 @@ partial class BeltManager : MonoBehaviour
     [SerializeField] GameObject beltLinekdPos2;
     [SerializeField] GameObject beltLinekdPos3;
 
-    [SerializeField] GameObject cam;
+    //[SerializeField] GameObject cam;
     bool isLinked;
 
     public void beltSelectEntered()
